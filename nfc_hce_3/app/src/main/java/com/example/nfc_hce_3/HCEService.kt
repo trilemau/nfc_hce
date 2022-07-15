fc67f2e1fb469204e5fca32335bad7da9d988cb2
trilemau <49056727+trilemau@users.noreply.github.com>
2022-07-15 17:54:36 +0200

    WIP random card string

--- nfc_hce_3/app/src/main/java/com/example/nfc_hce_3/HCEService.kt
+++ nfc_hce_3/app/src/main/java/com/example/nfc_hce_3/HCEService.kt
@@ -8,12 +8,13 @@ class HCEService : HostApduService() {
 
     object HCEConstants {
         const val TAG = "Host Card Emulator"
-        const val STATUS_SUCCESS = "9000"
+        const val STATUS_SELECT_SUCCESS = "9000"
         const val STATUS_FAILED = "6F00"
         const val CLA_NOT_SUPPORTED = "6E00"
         const val INS_NOT_SUPPORTED = "6D00"
         const val AID = "FF3F12C1583A69D28C4082412A90AC00"
         const val SELECT_INS = "A4"
+        const val GET_INS = "C0"
         const val DEFAULT_CLA = "00"
         const val MIN_APDU_LENGTH = 12
     }
@@ -41,9 +42,7 @@ class HCEService : HostApduService() {
             return Utility.hexStringToByteArray(HCEConstants.CLA_NOT_SUPPORTED)
         }
 
-        if (hexCommandApdu.substring(2, 4) != HCEConstants.SELECT_INS) {
-            return Utility.hexStringToByteArray(HCEConstants.INS_NOT_SUPPORTED)
-        }
+        if (hexCommandApdu.substring(2, 4) == HCEConstants.SELECT_INS) {
 
 //        if (hexCommandApdu.substring(10, 24) == HCEConstants.AID)  {
 //            return Utility.hexStringToByteArray(HCEConstants.STATUS_SUCCESS)
@@ -51,9 +50,19 @@ class HCEService : HostApduService() {
 //            return Utility.hexStringToByteArray(HCEConstants.STATUS_FAILED)
 //        }
 
-        Log.d("PROCESS_COMMAND_APDU", "RESPONSE SUCCESS")
+            Log.d("PROCESS_COMMAND_APDU", "SELECT SUCCESS")
 
-        return Utility.hexStringToByteArray(HCEConstants.STATUS_SUCCESS)
+//            return Utility.hexStringToByteArray(HCEConstants.STATUS_SELECT_SUCCESS)
+            return Utility.hexStringToByteArray("AAAAAAAA")
+        }
+
+        if (hexCommandApdu.substring(2, 4) == HCEConstants.GET_INS) {
+            Log.d("PROCESS_COMMAND_APDU", "GET SUCCESS")
+
+            return Utility.hexStringToByteArray("1234567890ABCDEEF")
+        }
+
+        return Utility.hexStringToByteArray(HCEConstants.INS_NOT_SUPPORTED)
     }
 
     // Different AID has been selected or lost NFC connection
